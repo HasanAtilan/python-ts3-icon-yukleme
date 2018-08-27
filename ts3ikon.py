7241561ceb32a539730a057cc6d62311a961f776
HasanAtilan <zb-sunucum@hotmail.com>
2018-08-27 19:14:30 +0300

    Create ts3ikon.py

---
+++ ts3ikon.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+with ts3.query.TS3Connection("localhost") as ts3conn:
+ts3conn.login(
+client_login_name="serveradmin", # Sunucu Kullanıcı Adı
+client_login_password="FoOBa9" # Sunucu Şifreniz
+)
+
+# Yeni Giriş Alıyoruz
+# Ts3 Girisi.
+
+ts3ft = ts3.filetransfer.TS3FileTransfer(ts3conn)
+
+#Resim Yüklüyoruz
+#Klasör Açıyoruz
+#Ardından Upload Yapıyoruz
+
+with open("resim.png", "resim2") as file:
+ts3ft.init_upload(input_file=file, name="/resim2.png", cid=2) # İsim ve Kullanici İD
