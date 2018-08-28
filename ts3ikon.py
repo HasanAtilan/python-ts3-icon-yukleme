a32896ed43768747f776b915a6d7f6cbd63c9c6d
HasanAtilan <zb-sunucum@hotmail.com>
2018-08-28 03:33:59 +0300

    Update ts3ikon.py

--- ts3ikon.py
+++ ts3ikon.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
 
-with ts3.query.TS3Connection("localhost") as ts3conn:
-ts3conn.login(
-client_login_name="serveradmin", # Sunucu Kullanıcı Adı
-client_login_password="FoOBa9" # Sunucu Şifreniz
+with ts3.query.TS3Connection("localhost") as ts3giris:
+ts3giris.girelim(
+kullanici_adi="serveradmin", # Sunucu Kullanıcı Adı
+yatqa_sifresi="FoOBa9" # Sunucu Şifreniz
 )
 
-# Yeni Giriş Alıyoruz
-# Ts3 Girisi.
-
-ts3ft = ts3.filetransfer.TS3FileTransfer(ts3conn)
+ts3dosya = ts3.filetransfer.TS3FileTransfer(ts3giris)
 
 #Resim Yüklüyoruz
 #Klasör Açıyoruz
 #Ardından Upload Yapıyoruz
 
-with open("resim.uzantisi", "klasor ismi") as file:
-ts3ft.init_upload(input_file=file, name="resim.uzantisi", cid=2) # Dosya Adı ve Kullanici İD'si
+with open("resim.uzantisi", "klasor ismi") as dosya:
+ts3dosya.init_upload(input_file=dosya, name="resim.uzantisi", cid=2) # Dosya Adı ve Kullanici İD'si
